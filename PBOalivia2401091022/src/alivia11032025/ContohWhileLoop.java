4ec74e090e26db4962c09bbbf7ee24900fdcb645
pboalivia2401091022 <alivia220220@gmail.com>
2025-03-13 10:06:26 +0700

    Add files via upload

---
+++ PBOalivia2401091022/src/alivia11032025/ContohWhileLoop.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alivia11032025;
+
+/**
+ *
+ * @author LABP1KOMP
+ */
+public class ContohWhileLoop {
+      public static void main( String[] args )
+ {
+    int x = 0;
+    while (x<10)
+{
+ System.out.println(x);
+ x++;
+}
+ 
+}
+      
+}
+    
+
